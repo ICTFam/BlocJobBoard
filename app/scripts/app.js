635dc558dab1e4cb4032f19058d04c7ef723cee4
Souphan <souphan.ctr@gmail.com>
2016-05-12 19:00:44 -0500

    added checkbox

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,6 +33,20 @@ blocJobs.config(function($stateProvider, $urlRouterProvider){
     })
 })
 
+//it('should change state', function() {
+//      var value1 = element(by.binding('checkboxModel.value1'));
+//      var value2 = element(by.binding('checkboxModel.value2'));
+//
+//      expect(value1.getText()).toContain('true');
+//      expect(value2.getText()).toContain('YES');
+//
+//      element(by.model('checkboxModel.value1')).click();
+//      element(by.model('checkboxModel.value2')).click();
+//
+//      expect(value1.getText()).toContain('false');
+//      expect(value2.getText()).toContain('NO');
+//    });
+
 blocJobs.config(function($authProvider) {
     $authProvider.facebook({
       clientId: 'Facebook App ID'
